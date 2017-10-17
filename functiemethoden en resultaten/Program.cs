20a7d23fd388501b422f04e0af116244b9078a55
Bos Mats <mats.bos@immalle.eu>
2017-10-17 09:40:54 +0200

    Add project files.

---
+++ functiemethoden en resultaten/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functiemethoden_en_resultaten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
